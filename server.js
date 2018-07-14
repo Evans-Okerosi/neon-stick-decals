00f2e7a93dbecfc47e074bd1ccd0669cae0eb419
Evans Okerosi <evansokerosi@gmail.com>
2018-07-14 17:41:36 +0300

    removed annoying airbnb style rules if favour of prettier

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-// eslint-disable-line 
-require("dotenv").load();// load environment config
+// eslint-disable-line
+require('dotenv').load(); // load environment config
 
 // eslint-disable-next-line
-const PORT = process.env.PORT || 8080; // set port 
-const express = require("express");
-const path = require("path");
-const logger = require("morgan");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const helmet = require("helmet");
-const passport = require("passport");
+const PORT = process.env.PORT || 8080; // set port
+const express = require('express');
+// const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const passport = require('passport');
 // require controllers
-const skin = require("./controllers/skin");
+const skin = require('./controllers/skin');
 
 const app = express();
 // middleware used in all routes
@@ -19,30 +19,34 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application.json" }));
+app.use(bodyParser.json({ type: 'application.json' }));
 app.use(cookieParser());
-app.use(logger("tiny"));
+app.use(logger('tiny'));
 
 // static files directories
 app.use(express.static(`${__dirname}/client/public`));
 app.use(passport.initialize());
 
 // passport strategies
-const localSignupStrategy = require("./passport/local-signup");
-const localLoginStrategy = require("./passport/local-login");
+const localSignupStrategy = require('./passport/local-signup');
+const localLoginStrategy = require('./passport/local-login');
+
 passport.use('local-signup', localSignupStrategy);
 passport.use('local-login', localLoginStrategy);
 
 // auth check middleware
 const authCheckMiddleware = require('./middleware/auth-check');
-app.use('', authCheckMiddleware);
-// ROUTES
-app.get("/", () => {
-});
-// skins
-app.use("/skin",skin);
-// static files route
 
+app.use('/user', authCheckMiddleware);
+// ROUTES
+const userInfo = require('./controllers/user/info');
+
+app.use('/user', userInfo);
+
+app.get('/', () => {});
+// skins
+app.use('/skin', skin);
+// static files route
 
 app.listen(PORT, () => {
   // eslint-disable-next-line
